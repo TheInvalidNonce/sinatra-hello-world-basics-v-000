c636819524e3454fd18dbaae580f229b26688933
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-18 18:07:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
-    resp.write "Hello, World!"
-    resp.status = 200
+    "Hello, World!"
   end
 
 end
